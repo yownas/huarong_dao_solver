d759f913f39771237cc3d98efc5b86ea3012a8ed
Jonas Stare <yownas@gmail.com>
2019-12-06 21:29:26 +0100

    Move old versions and just keep the best one

--- README.md
+++ README.md
@@ -14,6 +14,3 @@ The moves for the blocks and rotations are hard-coded but should work for any bo
 
 (I also made a Rush Hour solver since the problem is more or less the same.)
 
-````compact_solver.sh```` - Remove some checks to make the code smaller. Also let the sed script compress itself.
-
-````quick_solver.sh```` - Do not keep track of which piece is which. Makes the file that keeps track of different boards a lot smaller and solves the puzzle much quicker. The drawback, since we do not track the pieces, is that they may change name. We solve this by dazzling the user with pretty ansi colors instead.
--- solver.sh
+++ solver.sh
@@ -9,32 +9,26 @@ start="
 #I__J#  jonst@nsc.liu.se                         Show result from
 ##--##                                           previous run
 "
+i=0
 win="#AA#"
-
-m='s/.* //;h;p;s/__\(....\)AA\(....\)AA/AA\1AA\2__/;p;g;p;s/AA_\(...\)AA_/_AA\1_AA/;p;g;p;s/AA\(....\)AA\(....\)[_-][_-]/__\1AA\2AA/;p;g;p;s/_AA\(...\)_AA/AA_\1AA_/;p;g;p;s/AA\(....\)AA\(....\)__\(....\)--/__\1__\2AA\3AA/;p;g;p;s/_AA\(...\)_AA\(...\)--/___\1AA_\2AA/;p;g;p;s/AA_\(...\)AA_\(....\)--/___\1_AA\2AA/;p;g;p;s/_\(.....\)B\(.....\)B/B\1B\2_/;p;g;p;s/B_\(....\)B_/_B\1_B/;p;g;p;s/B\(.....\)B\(.....\)_/_\1B\2B/;p;g;p;s/_B\(....\)_B/B_\1B_/;p;g;p;s/_\(.....\)_\(.....\)B\(.....\)B/B\1B\2_\3_/;p;g;p;s/B\(.....\)B\(.....\)_\(.....\)_/_\1_\2B\3B/;p;g;p;s/_\(.....\)C\(.....\)C/C\1C\2_/;p;g;p;s/C_\(....\)C_/_C\1_C/;p;g;p;s/C\(.....\)C\(.....\)_/_\1C\2C/;p;g;p;s/_C\(....\)_C/C_\1C_/;p;g;p;s/_\(.....\)_\(.....\)C\(.....\)C/C\1C\2_\3_/;p;g;p;s/C\(.....\)C\(.....\)_\(.....\)_/_\1_\2C\3C/;p;g;p;s/_\(.....\)D\(.....\)D/D\1D\2_/;p;g;p;s/D_\(....\)D_/_D\1_D/;p;g;p;s/D\(.....\)D\(.....\)_/_\1D\2D/;p;g;p;s/_D\(....\)_D/D_\1D_/;p;g;p;s/_\(.....\)_\(.....\)D\(.....\)D/D\1D\2_\3_/;p;g;p;s/D\(.....\)D\(.....\)_\(.....\)_/_\1_\2D\3D/;p;g;p;s/_\(.....\)E\(.....\)E/E\1E\2_/;p;g;p;s/E_\(....\)E_/_E\1_E/;p;g;p;s/E\(.....\)E\(.....\)_/_\1E\2E/;p;g;p;s/_E\(....\)_E/E_\1E_/;p;g;p;s/_\(.....\)_\(.....\)E\(.....\)E/E\1E\2_\3_/;p;g;p;s/E\(.....\)E\(.....\)_\(.....\)_/_\1_\2E\3E/;p;g;p;s/__\(....\)FF/FF\1__/;p;g;p;s/FF_/_FF/;p;g;p;s/FF\(....\)__/__\1FF/;p;g;p;s/_FF/FF_/;p;g;p;s/__FF/FF__/;p;g;p;s/FF__/__FF/;p;g;p;s/_\(.....\)G/G\1_/;p;g;p;s/G_/_G/;p;g;p;s/G\(.....\)_/_\1G/;p;g;p;s/_G/G_/;p;g;p;s/_\(....._.....\)G/G\1_/;p;g;p;s/G\(....._.....\)_/_\1G/;p;g;p;s/__\(.....\)G/G_\1_/;p;g;p;s/G_\(.....\)_/__\1G/;p;g;p;s/_G\(....\)_/__\1G/;p;g;p;s/__\(....\)G/_G\1_/;p;g;p;s/G__/__G/;p;g;p;s/__G/G__/;p;g;p;s/G\(....\)__/_\1_G/;p;g;p;s/_\(....\)_G/G\1__/;p;g;p;s/G\(.....\)__/_\1_G/;p;g;p;s/_\(.....\)_G/G\1__/;p;g;p;s/_\(.....\)H/H\1_/;p;g;p;s/H_/_H/;p;g;p;s/H\(.....\)_/_\1H/;p;g;p;s/_H/H_/;p;g;p;s/_\(....._.....\)H/H\1_/;p;g;p;s/H\(....._.....\)_/_\1H/;p;g;p;s/__\(.....\)H/H_\1_/;p;g;p;s/H_\(.....\)_/__\1H/;p;g;p;s/_H\(....\)_/__\1H/;p;g;p;s/__\(....\)H/_H\1_/;p;g;p;s/H__/__H/;p;g;p;s/__H/H__/;p;g;p;s/H\(....\)__/_\1_H/;p;g;p;s/_\(....\)_H/H\1__/;p;g;p;s/H\(.....\)__/_\1_H/;p;g;p;s/_\(.....\)_H/H\1__/;p;g;p;s/_\(.....\)I/I\1_/;p;g;p;s/I_/_I/;p;g;p;s/I\(.....\)_/_\1I/;p;g;p;s/_I/I_/;p;g;p;s/_\(....._.....\)I/I\1_/;p;g;p;s/I\(....._.....\)_/_\1I/;p;g;p;s/__\(.....\)I/I_\1_/;p;g;p;s/I_\(.....\)_/__\1I/;p;g;p;s/_I\(....\)_/__\1I/;p;g;p;s/__\(....\)I/_I\1_/;p;g;p;s/I__/__I/;p;g;p;s/__I/I__/;p;g;p;s/I\(....\)__/_\1_I/;p;g;p;s/_\(....\)_I/I\1__/;p;g;p;s/I\(.....\)__/_\1_I/;p;g;p;s/_\(.....\)_I/I\1__/;p;g;p;s/_\(.....\)J/J\1_/;p;g;p;s/J_/_J/;p;g;p;s/J\(.....\)_/_\1J/;p;g;p;s/_J/J_/;p;g;p;s/_\(....._.....\)J/J\1_/;p;g;p;s/J\(....._.....\)_/_\1J/;p;g;p;s/__\(.....\)J/J_\1_/;p;g;p;s/J_\(.....\)_/__\1J/;p;g;p;s/_J\(....\)_/__\1J/;p;g;p;s/__\(....\)J/_J\1_/;p;g;p;s/J__/__J/;p;g;p;s/__J/J__/;p;g;p;s/J\(....\)__/_\1_J/;p;g;p;s/_\(....\)J_/J\1__/;p;g;p;s/J\(.....\)__/_\1_J/;p;g;p;s/_\(.....\)_J/J\1__/;p'
-
 db="/tmp/data.db"
-sbf=4G # Sort Buffer Size
+c='s/.* #/#/;s#\(.\)\([0-9]*\)#s/\1/e[\2m  /g;#g; #100A102B104F105G103_49-49$49'
+m='sS.*#SS;sSaSU;p;g;p;sUSg;sSbS\\(.....Sg;sScS\\(....Sg;sSdS_.....\\)Sg;sSeS\\)Sg;sSfS_U_Sg;sS\([1-3]\)S\\\1Sg#sU.* UU;sUBbeBUC1CU;sUBbeBUD1DU;sUBbeBUE1EU;sUGUHU;sUGUIU;sUGUJU;h;p;sU__ceAAceAAUAA1AA2__aAA_\(...eAAfAA1_AAaAAceAAce[_-][_-]U__1AA2AAa_AA\(...e_AAUAA_1AA_aAAceAAce__ce--U__1__2AA3AAa_AA\(...e_AA\(...e--U___1AA_2AAaAA_\(...eAA_ce--U___1_AA2AAa_beBbeBUB1B2_aB_ceBfB1_BaBbeBbef1B2Ba_Bce_BUB_1B_a_be_beBbeBUB1B2_3_aBbeBbe_bef1_2B3Ba_beCbeCUC1C2_aC_ceCfC1_CaCbeCbef1C2Ca_Cce_CUC_1C_a_be_beCbeCUC1C2_3_aCbeCbe_bef1_2C3Ca_beDbeDUD1D2_aD_ceDfD1_DaDbeDbef1D2Da_Dce_DUD_1D_a_be_beDbeDUD1D2_3_aDbeDbe_bef1_2D3Da_beEbeEUE1E2_aE_ceEfE1_EaEbeEbef1E2Ea_Ece_EUE_1E_a_be_beEbeEUE1E2_3_aEbeEbe_bef1_2E3Ea__ceFFUFF1__aFFfFFaFFce_f_1FFa_FFUFF_a__FFUFF__aFF_f_FFa_beGUG1_aGfGaGbef1Ga_GUG_a_bdGUG1_aGbdf1Ga__beGUG_1_aG_bef_1Ga_Gcef_1Ga__ceGU_G1_aG_f_Ga__GUG__aGce_f1G_a_ceG_UG1__aGbe_f1_Ga_be_GUG1__a_beHUH1_aHfHaHbef1Ha_HUH_a_bdHUH1_aHbdf1Ha__beHUH_1_aH_bef_1Ha_Hcef_1Ha__ceHU_H1_aH_f_Ha__HUH__aHce_f1H_a_ceH_UH1__aHbe_f1_Ha_be_HUH1__a_beIUI1_aIfIaIbef1Ia_IUI_a_bdIUI1_aIbdf1Ia__beIUI_1_aI_bef_1Ia_Icef_1Ia__ceIU_I1_aI_f_Ia__IUI__aIce_f1I_a_ceI_UI1__aIbe_f1_Ia_be_IUI1__a_beJUJ1_aJfJaJbef1Ja_JUJ_a_bdJUJ1_aJbdf1Ja__beJUJ_1_aJ_bef_1Ja_Jcef_1Ja__ceJU_J1_aJ_f_Ja__JUJ__aJce_f1J_a_ceJ_UJ1__aJbe_f1_Ja_be_JUJ1__U;p'
+m=$(sed "$m" <<< "$m")
 
 case "$1" in
   solve)
-    # Initialize data
-    echo "0 Start $(echo $start|tr -cd 'A-J#_-')" > $db 
-    echo "" > ${db}t
-
-    i=0
-    until grep "$win" ${db}t > /dev/null; do # Are we there yet?
+    # Initialize database
+    echo "0 # $(tr -cd 'A-J#_-' <<< $start)" > $db
+    until grep -q "$win" $db; do # Are we there yet?
       l=$(( i++ ))
       echo -n "### $i: "
       # Try all moves on the new board patterns | keep those who make a change
-      grep "^$l " $db | sed -n "$m" | awk '{a=$0;getline;b=$0;if(a!=b){print '$i',a,b}}' > ${db}t
-      if [ \! -s ${db}t ];then echo "No new moves?";exit 255;fi
-      # Save only results that doesn't already exist in db 
-      cat $db ${db}t | sort -S$sbf -u -k3,3 | sort -S$sbf -n -k1 > ${db}r
+      (cat $db ; grep "^$l " $db | sed -n "$m" | tr CDEHIJ BBBGGG | awk '{a=$0;getline;if(a!=$0){print '$i',a,$0}}') | sort -uk3,3 > ${db}r
       mv -f ${db}r $db
 
       # Show db size (Just to see if anything is happening)
-      wc -c $db | cut -d' ' -f1
+      wc -l < $db
     done
     ;;
   show)
@@ -47,5 +41,4 @@ esac
 
 # Display all the steps
 echo "Solution:"
-f='/'$win'/{if(!p){p=$2;print $1,$3}}{if($3==p){print $1,p;p=$2}}'
-tac $db | awk "$f" | rev | sed 'y/ /\n/;s/\(#....#\)/\n\1/g' | tac | rev
+sort -rnk1 < $db | awk '/'$win'/{if(!p){p=$2;print $1,$3}}{if($3==p){print $1,p;p=$2}}' | rev | sed 'y/ /\n/;s/\(#....#\)/\n\1/g' | tac | rev | sed "$(sed "$c" <<< $c)" | tr 'e' '\033'
