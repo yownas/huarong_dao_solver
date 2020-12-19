e285f7b21287b005b8c320a8c3230e3d15055518
Jonas Stare <yownas@gmail.com>
2020-12-19 23:32:21 +0100

    Small clean up

--- mini_solver.sh
+++ mini_solver.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ######  Klotski/huarong dao solver
+#BAAC#  <- Solves this setup in 81 steps
 #BAAC#
-#BAAC#  Would probably be quicker if it
-#DFFE#  was written in a proper language.
-#DGHE#
+#DFFE#  Would probably be quicker if it
+#DGHE#  was written in a proper language.
 #I__J#  jonst@nsc.liu.se
 ##--##
 
-w="#AA#";d="/tmp/data.db"; s="CDEHIJ BBBGGG"; c='s/.* #/#/;s#\(.\)\([0-9]*\)#s/\1/e[\2m  /g;#g; #100A102B104F105G103_49-49$49'
+w="#AA#"; d="/tmp/data.db"; s="CDEHIJ BBBGGG"; c='s/.* #/#/;s#\(.\)\([0-9]*\)#s/\1/e[\2m  /g;#g; #100A102B104F105G103_49-49$49'
 m='s/^.* #v//;s/x/\\1/g;s/w/\\2/g;s/v/\/g;s\//g; #vs/^.* #//;s/a\([BCDE]\)/a5x5xUx7xefxtx6xbx0x5x5kxexax8_xUx_7xi5lx5xUx7xox5xrx\\3xv\([^Um]\)\([GHIJ]\)/xwUwnw6w0w5kwawUwi9wUwnw9kwalwUw_nwljwaw8jwatwU_wnw_6_wa_wUw_fw8_kwi8w_Uwbfw5_6bwa5_wUwbvu/FFvt/_8vr/5l6bevq/B5BUvo/e_\\3fvn/b0vm/U;sUvl/_5vk/67vi/_avj/6_7vf/_0ve/\wvd/\\(...\\)vc/AAvb/7_va/0_v9/\\(....._.4v8/\\(4v7/\xv6/_U_v5/\\(.4v4/....\\)v0/U;p;g;p;sU/g; #sU.* UmqC7CmqD7DmqE7EmGUHmGUImGUJU;h;p;sU_tc8cUc7ce_fc_dc6cbc0c8c8[_-][_-]U__7cecacd_cUc_7cfc8c8_t--U__b_ec\3cacd_cd--U___7c_ec0c_dct--U___bcecaBaCaDaEatuUubfu6u0u8_juauUui_uUu_fu_6_ua5Gi5Hi5Ii5J_U;p'
 m=$(sed "$(sed "$m" <<< "$m")" <<< "$m")
 
